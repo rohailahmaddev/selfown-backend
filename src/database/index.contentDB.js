eb88904b5de307760f32e459afc5d09a3d34315d
Rohail Ahmad <rohailrao07@gmail.com>
2026-05-14 20:59:51 +0500

    update sql connection

--- src/database/index.contentDB.js
+++ src/database/index.contentDB.js
@@ -3,11 +3,11 @@ import mysql from "mysql2/promise"
 
 console.log(process.env.MYSQLHOST)
 const pool = mysql.createPool({
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  port: Number(process.env.MYSQLPORT),
+  host: 'mysql.railway.internal',
+  user: 'root',
+  password: 'NUnSqRoIphhaxTMXKNUgnXKgNdovyFYz',
+  database: 'railway',
+  port: 3306,
   ssl: {
     rejectUnauthorized: false
   }
