04420ec447bc904d5f33291bc25ef8e40a286d28
Rohail Ahmad <rohailrao07@gmail.com>
2026-05-14 19:15:08 +0500

    Change sql credentials

--- src/database/index.contentDB.js
+++ src/database/index.contentDB.js
@@ -1,10 +1,10 @@
 import mysql from "mysql2/promise"
 
 const pool = mysql.createPool({
-    host: process.env.HOST || 'localhost',
-    user: process.env.USER || 'root',
-    password: process.env.PASSWORD || '',
-    database: process.env.DATABASE || 'blog_application_db'
+    host: process.env.HOST ,
+    user: process.env.USER ,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE
 })
 
 export default pool
--- src/index.js
+++ src/index.js
@@ -11,8 +11,6 @@ dotenv.config({
 Scheme()
 
 
-const PORT = process.env.PORT || 4500
+const PORT = process.env.PORT || 8080
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`)
-})
+app.listen(PORT)
