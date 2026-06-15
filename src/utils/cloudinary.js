e0a94687d89bd056e0289cbead41209cea853448
Rohail Ahmad <rohailrao07@gmail.com>
2026-06-15 22:35:56 +0500

    update cloudinary config

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -6,7 +6,7 @@ const UPLOAD_TIMEOUT = 30000;
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  api_secret: process.env.CLOUDINARY_SECRET_KEY,
 });
 console.log(process.env.CLOUD_NAME);
 console.log(process.env.CLOUDINARY_API_KEY);
