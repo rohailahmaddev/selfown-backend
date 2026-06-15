d4af9aa60679ecc7cb3ab3ac3c42c940665e41fc
Rohail Ahmad <rohailrao07@gmail.com>
2026-06-15 22:31:55 +0500

    cloudinary config

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -3,6 +3,15 @@ import streamifier from "streamifier";
 
 const UPLOAD_TIMEOUT = 30000;
 
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+console.log(process.env.CLOUD_NAME);
+console.log(process.env.CLOUDINARY_API_KEY);
+console.log(process.env.CLOUDINARY_API_SECRET);
+
 export const uploadOnCloudinary = (buffer, folder = "uploads") => {
   return new Promise((resolve, reject) => {
     // Input validation
