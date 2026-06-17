f6bbee90e0dbfe5852e2e3d36a39f4f5a06099c5
Rohail Ahmad <rohailrao07@gmail.com>
2026-06-17 15:07:03 +0500

    update cloudinary controller

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -8,9 +8,6 @@ cloudinary.config({
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_SECRET_KEY,
 });
-console.log(process.env.CLOUD_NAME);
-console.log(process.env.CLOUDINARY_API_KEY);
-console.log(process.env.CLOUDINARY_SECRET_KEY);
 
 export const uploadOnCloudinary = (buffer, folder = "uploads") => {
   return new Promise((resolve, reject) => {
@@ -51,30 +48,24 @@ export const uploadOnCloudinary = (buffer, folder = "uploads") => {
     const stream = cloudinary.uploader.upload_stream(
       { resource_type: "auto", folder },
       (error, result) => {
-        console.log("Callback reached");
     
         if (error) {
-          console.log("Cloudinary Error:", error);
           return safeReject(error);
         }
     
-        console.log("Cloudinary Success:", result);
         safeResolve(result);
       }
     );
     
     stream.on("error", (err) => {
-      console.log("Stream Error:", err);
       safeReject(err);
     });
     
     readStream.on("error", (err) => {
-      console.log("Read Stream Error:", err);
       safeReject(err);
     });
     
     readStream.on("end", () => {
-      console.log("Read Stream Ended");
     });
     
     readStream.pipe(stream);
